1d658ccd6854406330c9ae5c3faccd45aa392023
Aneprados <apradoscamara@gmail.com>
2025-03-31 17:32:24 +0100

    lanzador creado

--- Rectangulos_clase.py
+++ Rectangulos_clase.py
@@ -30,3 +30,25 @@ class Rectangulo:
         :return: El área del rectángulo.
         """
         return self.base() * self.altura()
+        @classmethod
+        def crear_con_puntos(cls, punto_inicial=None, punto_final=None):
+            """
+            Método de clase para crear un rectángulo a partir de dos puntos.
+            :param punto_inicial: Instancia de la clase Punto.
+            :param punto_final: Instancia de la clase Punto.
+            :return: Una instancia de Rectangulo.
+            """
+            return cls(punto_inicial, punto_final)
+    def crear_con_puntos(cls, x1=0, y1=0, x2=0, y2=0):
+            """
+            Método de clase para crear un rectángulo a partir de coordenadas.
+            Si no se reciben coordenadas, se inicializan en cero.
+            :param x1: Coordenada x del punto inicial.
+            :param y1: Coordenada y del punto inicial.
+            :param x2: Coordenada x del punto final.
+            :param y2: Coordenada y del punto final.
+            :return: Una instancia de Rectangulo.
+            """
+            punto_inicial = Punto(x1, y1)
+            punto_final = Punto(x2, y2)
+            return cls(punto_inicial, punto_final)
